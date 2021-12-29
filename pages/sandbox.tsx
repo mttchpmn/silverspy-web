35e222736e5107813364f01a9375a8b8fbc45a04
Matt Chapman <matt@mattchapman.io>
2021-12-30 12:39:38 +1300

    Init commit

---
+++ pages/sandbox.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import {Button} from "antd";
+
+const Sandbox: NextPage = () => {
+    return (
+        <div>
+            <Button>Hello world</Button>
+        </div>
+    );
+}
+
+export default Sandbox
