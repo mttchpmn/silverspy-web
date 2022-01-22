cef4abc54566f09b8947b66d180f0800261a379d
Matt Chapman <matt@mattchapman.io>
2022-01-22 22:24:41 +1300

    Simplify Transactions table

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -1,55 +1,87 @@
-import {Transaction} from "../types/transaction-types";
-import {EditableTable} from "./EditableTable";
+import { Transaction } from "../types/transaction-types";
 import moment from "moment";
-import {DeliveredProcedureOutlined, EditOutlined} from "@ant-design/icons";
-
+import {
+  DeliveredProcedureOutlined,
+  EditOutlined,
+  UpCircleOutlined,
+  DownCircleOutlined,
+} from "@ant-design/icons";
+import { Table } from "antd";
+import { ReactElement } from "react";
 
 type TransactionTableProps = {
-    transactionData: Transaction[];
-    onRowUpdate: (row: any) => Promise<boolean>
-}
+  transactionData: Transaction[];
+  onRowUpdate: (row: any) => Promise<boolean>;
+};
 
-export const TransactionsTable = ({transactionData, onRowUpdate}: TransactionTableProps) => {
-    console.log({transactionData})
-    const columns = [
-        {
-            title: "Date",
-            dataIndex: "date",
-            key: "date",
-            width: "10%",
-            render: (date: string) => moment(date).format("ddd MMM Do")
-        },
-        {
-            title: "Type",
-            dataIndex: "type",
-            key: "type",
-            width: "10%",
-            render: (x: number) => x > 0 ? "CREDIT" : "DEBIT"
-        },
-        {
-            title: "Category",
-            dataIndex: "category",
-            key: "category",
-            width: "10%",
-            editable: true
-        },
-        {
-            title: "Description",
-            dataIndex: "description",
-            key: "description",
-            editable: true
-            // width: "10%"
-        },
-        {
-            title: "Value",
-            dataIndex: "value",
-            key: "value",
-            width: "20%",
-            render: (x: number) => `$${x}`
-        },
-    ];
+export const TransactionsTable = ({
+  transactionData,
+  onRowUpdate,
+}: TransactionTableProps) => {
+  console.log({ transactionData });
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      width: "10%",
+      render: (date: string) => moment(date).format("ddd MMM Do"),
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+      width: "5%",
+      render: (x: number) =>
+        x === 1 ? (
+          <UpCircleOutlined style={{ color: "green" }} />
+        ) : (
+          <DownCircleOutlined style={{ color: "red" }} />
+        ),
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+      width: "10%",
+    },
+    {
+      title: "Details",
+      dataIndex: "details",
+      key: "details",
+      render: (val, record) => (val ? val : <em>{record.description}</em>),
+    },
+    // {
+    //   title: "Description",
+    //   dataIndex: "description",
+    //   key: "description",
+    // },
+    {
+      title: "Value",
+      dataIndex: "value",
+      key: "value",
+      width: "10%",
+      render: (x: number) => `$${x}`,
+    },
+    {
+      title: "Edit",
+      dataIndex: "edit",
+      key: "edit",
+      width: "5%",
+      render: (_: any, record: any): ReactElement => (
+        <EditOutlined onClick={() => console.log("EDIT")} />
+      ),
+    },
+  ];
 
-    return <div>
-        <EditableTable columns={columns} inputData={transactionData} onRowUpdate={onRowUpdate}/>
+  return (
+    <div>
+      <Table
+        bordered
+        size={"small"}
+        columns={columns}
+        dataSource={transactionData}
+      />
     </div>
-}
+  );
+};
