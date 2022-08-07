9350c08aec3e0021ef09a01bb6b6c47c699a868e
Matt Chapman <matt@mattchapman.io>
2022-08-07 14:29:40 +1200

    Add 'coming soon' text

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,18 @@
-import type {NextPage} from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {PageContainer} from "../components/PageContainer";
-import {withPageAuthRequired} from "@auth0/nextjs-auth0";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { PageContainer } from "../components/PageContainer";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 const Home: NextPage = () => {
-    return (
-        <PageContainer title={"Dashboard"}>
-            Dashboard page
-        </PageContainer>
-    )
-}
+  return (
+    <PageContainer title={"Dashboard"}>
+      <h2>Coming soon!</h2>
+    </PageContainer>
+  );
+};
 
 export const getServerSideProps = withPageAuthRequired();
 
-export default Home
+export default Home;
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,29 +1,31 @@
-import {NextPage} from "next";
-import {PageContainer} from "../components/PageContainer";
-import {useUser, withPageAuthRequired} from "@auth0/nextjs-auth0";
+import { NextPage } from "next";
+import { PageContainer } from "../components/PageContainer";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 const Profile = () => {
-    const {user, error, isLoading} = useUser();
+  const { user, error, isLoading } = useUser();
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-    return (
-        user ? (
-            <div>
-                {/*<img src={user.picture} alt={user.name}/>*/}
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-            </div>
-        ) : null
-    );
-}
-const SettingsPage: NextPage = () =>
-{
-    return (
-        <PageContainer title={"Settings"}><Profile /></PageContainer>
-    )
-}
+  console.log({ user, error, isLoading });
+
+  return user ? (
+    <div>
+      {/*<img src={user.picture} alt={user.name}/>*/}
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  ) : null;
+};
+const SettingsPage: NextPage = () => {
+  return (
+    <PageContainer title={"Settings"}>
+      <h2>Coming soon!</h2>
+      {/* <Profile /> */}
+    </PageContainer>
+  );
+};
 
 export const getServerSideProps = withPageAuthRequired();
 
